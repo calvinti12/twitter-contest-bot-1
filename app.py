b3e92016f8842cc37a9bfe0c33ec74b16fa42c12
Carl Topham <carl.topham@therealbuzzgroup.com>
2017-02-11 13:28:49 +0000

    Tidy up

--- app.py
+++ app.py
@@ -8,23 +8,25 @@ from celery.task import periodic_task
 from twitter import Twitter
 from post_queue import PostQueue
 from ignore_list import IgnoreList
-
 from log import *
 
-twitter = Twitter(consumer_key, consumer_secret,
-                 access_token_key, access_token_secret,
-                 min_ratelimit, min_ratelimit_search, min_ratelimit_retweet)
-
-ignore = IgnoreList()
-queue = PostQueue()
-
+# Setup Flask App
 app = Flask(__name__, static_url_path='')
 app.config['CELERY_BROKER_URL'] = celery_broker_url
 app.config['CELERY_RESULT_BACKEND'] = celery_broker_url
 
+# Setup Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+# Create queues, and twitter object
+twitter = Twitter(consumer_key, consumer_secret,
+                 access_token_key, access_token_secret,
+                 min_ratelimit, min_ratelimit_search, min_ratelimit_retweet)
+ignore = IgnoreList()
+queue = PostQueue()
+
+#Schedule tasks with beat
 @periodic_task(run_every=timedelta(seconds=rate_limit_update_time))
 def CheckRateLimit():
     twitter.updateRateLimitStatus()
@@ -47,23 +49,22 @@ def UpdateQueue():
 
             queue.popFirst()
 
+
 @periodic_task(run_every=timedelta(seconds=scan_update_time))
 # Scan for new contests, but not too often because of the rate limit.
 def ScanForContests():
 
+    # Check if thw twitter object (rate limit) allows this roundof searches
     if twitter.canSearch():
 
-        print("=== SCANNING FOR NEW CONTESTS ===")
-
+        # Loop through the search queries
         for search_query in search_queries:
 
-            print("Getting new results for: " + search_query)
-
             try:
-                r = twitter.search(search_query)
+                query_results = twitter.search(search_query)
                 c = 0
 
-                for item in r:
+                for item in query_results:
 
                     c = c + 1
                     user_item = item['user']
@@ -113,35 +114,10 @@ def ScanForContests():
                                 ignore.add(id)
                                 print "blocked keywords - not adding"
 
-                                # else:
-                                #
-                                #
-                                # 	if is_retweet:
-                                # 		print(id + " ignored: " + original_screen_name + " on ignore list")
-                                # 	else:
-                                # 		print(original_screen_name + " in ignore list")
-
-                                # else:
-                                #
-                                # 	if is_retweet:
-                                # 		print(id + " ignored: " + original_id + " on ignore list")
-                                # 	else:
-                                # 		print(id + " in ignore list")
-
-                                # print("Got " + str(c) + " results")
-
             except Exception as e:
                 print(
                 "Could not connect to TwitterAPI - are your credentials correct?")
                 print("Exception: " + str(e))
-
-    else:
-
-        print("Search skipped! Queue: " + str(
-            queue.count()) + " Ratelimit: " + str(
-            twitter.rate_limit_search[1]) + "/" + str(
-            ratelimit_search[0]) + " (" + str(
-            ratelimit_search[2]) + "%)")
 
 
 # Check if a post requires you to follow the user.
@@ -209,8 +185,8 @@ def CheckForBlockedKeywords(item):
 
 @app.route('/')
 def root():
-    # task = my_background_task.apply_async(args=[10, 20], countdown=10)
     return render_template('index.html')
+
 
 @app.route('/tweetbot/api/v1.0/status', methods=['GET'])
 def get_status():
